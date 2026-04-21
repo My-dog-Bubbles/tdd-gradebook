b385751c5b44734fc8ec6985a0826a91390ce2d3
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-04-21 08:31:03 -0400

    fully implemented letter grade

--- gradebook.py
+++ gradebook.py
@@ -1,9 +1,20 @@
 def letter_grade(score):
-    return "A"
+    if score >= 90:
+        return "A"
+    if score >= 80:
+        return "B"
+    if score >= 70:
+        return "C"
+    if score >= 60:
+        return "D"
+    else:
+        return "F"
 
 def is_passing(score):
     pass
+
 def average(scores):
     pass
+    
 def curve_score(score, bonus):
     pass
