36f49cabf04deef5975ff49965dfb303a7a8a625
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-04-22 08:19:29 -0400

    fixed average

--- gradebook.py
+++ gradebook.py
@@ -18,7 +18,7 @@ def is_passing(score):
     return score >= 60
 
 def average(scores):
-    return round(sum(scores)/len(score),2)
+    return round(sum(scores)/len(scores),2)
     
 def curve_score(score, bonus):
     pass
